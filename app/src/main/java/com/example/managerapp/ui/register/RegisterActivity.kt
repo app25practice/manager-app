a2095d976cdc9342263514c76e8e04ecd340d884
changukLee <ckddnr5527@gmail.com>
2024-09-10 23:02:13 +0900

    design: 자격증명서 인증화면 디자인 추가

--- app/src/main/java/com/example/managerapp/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/managerapp/ui/register/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.managerapp.ui.register
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.managerapp.R
@@ -11,6 +12,15 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navigateToCertificate()
+    }
+
+    private fun navigateToCertificate() {
+        binding.goCertificationBtn.setOnClickListener {
+            val intent = Intent(this, CertificateActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
