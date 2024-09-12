976ee229ced27a222fe0d5e911cc952b43d50464
Jeonghoon Park <hoondb@naver.com>
2024-09-13 00:05:50 +0900

    feat: 실시간 동행 현황 내역이 비어 있을 경우 숨기기 기능 추가

--- app/src/main/java/com/example/managerapp/ui/companion/LiveCompanionActivity.kt
+++ app/src/main/java/com/example/managerapp/ui/companion/LiveCompanionActivity.kt
@@ -2,10 +2,17 @@ package com.example.managerapp.ui.companion
 
 import LiveCompanionRecyclerViewAdapter
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.managerapp.R
 import com.example.managerapp.databinding.ActivityLiveCompanionBinding
+import com.example.managerapp.ui.status.CompanionCompleteDialog
+import com.example.managerapp.ui.status.ReservationApplyRecyclerViewAdapter
+import com.example.managerapp.ui.status.ReservationDetails
+import com.example.managerapp.ui.status.ReservationInfo
+import com.example.managerapp.ui.status.UserInfo
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.LatLng
@@ -15,15 +22,34 @@ import com.kakao.vectormap.label.LabelOptions
 import com.kakao.vectormap.label.LabelStyle
 import com.kakao.vectormap.label.LabelStyles
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 class LiveCompanionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLiveCompanionBinding
     private var adapter = LiveCompanionRecyclerViewAdapter()
 
-    private var samplePatientName = "이수홍"
     private var sampleLatitude = 37.55
     private var sampleLongitude = 126.98
+
+    // 테스트 용 예약 데이터
+    private var mock = ReservationInfo(
+        UserInfo(
+            id = 7,
+            name = "문경자",
+            gender = "여",
+            birth = "640630",
+            phoneNumber = "01012345678"
+        ),
+        ReservationDetails(
+            reservationId = 7000,
+            date = Date(),
+            transportation = "택시",
+            message = "없음",
+            departureLocation = "부산 남구",
+            arrivalLocation = "부산대학교 병원"
+        )
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +61,7 @@ class LiveCompanionActivity : AppCompatActivity() {
         setManagerStatus()
         setLiveCompanionRecyclerViewAdapter()
         setCompanionStatusInputFormClickListener()
+        setCompanionCompleteBtnClickListener()
         navigateToPrevious()
     }
 
@@ -46,7 +73,7 @@ class LiveCompanionActivity : AppCompatActivity() {
     }
 
     private fun setManagerStatus() {
-        binding.patientNameTextView.text = samplePatientName
+        binding.patientNameTextView.text = mock.userInfo.name
     }
 
     private fun setCompanionStatusInputFormClickListener() {
@@ -59,6 +86,14 @@ class LiveCompanionActivity : AppCompatActivity() {
             adapter.submitList(currentList)
 
             binding.companionStatusInputEditText.text.clear()
+        }
+    }
+
+    // 다이얼 로그에 예약 정보도 같이 전달
+    private fun setCompanionCompleteBtnClickListener() {
+        binding.completeCompanionBtn.setOnClickListener {
+            val companionCompleteDialog = CompanionCompleteDialog(this, mock)
+            companionCompleteDialog.show()
         }
     }
 
@@ -110,8 +145,39 @@ class LiveCompanionActivity : AppCompatActivity() {
     private fun setLiveCompanionRecyclerViewAdapter() {
         binding.liveCompanionRecyclerView.adapter = adapter
         binding.liveCompanionRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        if (adapter.itemCount == 0)
+            binding.liveCompanionRecyclerView.visibility = View.GONE
+
+        setLiveCompanionAdapterDataObserver()
     }
 
+    private fun setLiveCompanionAdapterDataObserver() {
+        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onChanged() {
+                super.onChanged()
+                checkEmptyList()
+            }
+
+            override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+                super.onItemRangeRemoved(positionStart, itemCount)
+                checkEmptyList()
+            }
+
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                super.onItemRangeInserted(positionStart, itemCount)
+                checkEmptyList()
+            }
+        })
+    }
+
+    private fun checkEmptyList() {
+        if (adapter.itemCount == 0) {
+            binding.liveCompanionRecyclerView.visibility = View.GONE
+        } else {
+            binding.liveCompanionRecyclerView.visibility = View.VISIBLE
+        }
+    }
 
     private fun navigateToPrevious() {
         binding.mapPreviousBtn.setOnClickListener {
